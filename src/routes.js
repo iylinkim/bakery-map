a5765e21432c6174f386cf82d47da662f18dd659
Iylin <kiylin94@naver.com>
2021-01-04 23:37:04 +0900

    add Router

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import Home from './components/home/home';
+
+const AppRouter = (props) => (
+       <Router>
+           <Switch>
+               <Route path='/'>
+                    <Home/>
+               </Route>
+           </Switch>
+       </Router>   
+    );
+
+export default AppRouter;
